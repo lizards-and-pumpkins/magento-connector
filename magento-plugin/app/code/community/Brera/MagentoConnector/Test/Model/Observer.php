43bd1bcf81c5129c56e260c22d45246472803bdc
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-02-24 17:40:43 +0100

    Issue #1: refactor observer to reuse code

--- magento-plugin/app/code/community/Brera/MagentoConnector/Test/Model/Observer.php
+++ magento-plugin/app/code/community/Brera/MagentoConnector/Test/Model/Observer.php
@@ -7,30 +7,11 @@ class Brera_MagentoConnector_Test_Model_Observer extends EcomDev_PHPUnit_Test_Ca
      */
     public function saveProductIdOnSave()
     {
-        $product = new Varien_Object();
-        $productId = 12;
         $action = Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_CREATE_AND_UPDATE;
-        $product->setData(
-            array('id' => $productId)
-        );
-        $event = new Varien_Event_Observer();
-        $event->setData(
-            array(
-                'product' => $product,
-            )
-        );
+        $productId = 12;
+        $event = $this->createEventObserver($productId);
 
-        $productQueue = $this->getModelMock('brera_magentoconnector/product_queue_item', array('setProductId', 'setAction', 'save'));
-        $productQueue->expects($this->once())
-            ->method('setProductId')
-            ->with($this->equalTo($productId))
-            ->will($this->returnSelf());
-        $productQueue->expects($this->once())
-            ->method('setAction')
-            ->with($this->equalTo($action))
-            ->will($this->returnSelf());
-        $productQueue->expects($this->once())
-            ->method('save');
+        $productQueue = $this->mockProductQueue($productId, $action);
 
         $this->replaceByMock('model', 'brera_magentoconnector/product_queue_item', $productQueue);
 
@@ -43,6 +24,58 @@ class Brera_MagentoConnector_Test_Model_Observer extends EcomDev_PHPUnit_Test_Ca
      */
     public function saveProductIdOnDelete()
     {
+        $action = Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_DELETE;
+        $productId = 12;
+        $event = $this->createEventObserver($productId);
 
+        $productQueue = $this->mockProductQueue($productId, $action);
+
+        $this->replaceByMock('model', 'brera_magentoconnector/product_queue_item', $productQueue);
+
+        $observer = new Brera_MagentoConnector_Model_Observer();
+        $observer->catalogProductDeleteAfter($event);
+    }
+
+    /**
+     * @param $productId
+     * @param $action
+     * @return EcomDev_PHPUnit_Mock_Proxy
+     */
+    private function mockProductQueue($productId, $action)
+    {
+        $productQueue = $this->getModelMock('brera_magentoconnector/product_queue_item',
+            array('setProductId', 'setAction', 'save'));
+        $productQueue->expects($this->once())
+            ->method('setProductId')
+            ->with($this->equalTo($productId))
+            ->will($this->returnSelf());
+        $productQueue->expects($this->once())
+            ->method('setAction')
+            ->with($this->equalTo($action))
+            ->will($this->returnSelf());
+        $productQueue->expects($this->once())
+            ->method('save');
+
+        return $productQueue;
+    }
+
+    /**
+     * @param $productId
+     * @return Varien_Event_Observer
+     */
+    private function createEventObserver($productId)
+    {
+        $product = new Varien_Object();
+        $product->setData(
+            array('id' => $productId)
+        );
+        $event = new Varien_Event_Observer();
+        $event->setData(
+            array(
+                'product' => $product,
+            )
+        );
+
+        return $event;
     }
 }
