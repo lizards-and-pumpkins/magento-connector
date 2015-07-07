6b8d03d25a64c387a9ceceb3ee0776bdb5d2dd96
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-07-07 17:20:45 +0200

    Issue #11: Cleanup code duplication

--- magento-plugin/app/code/community/Brera/MagentoConnector/Model/Observer.php
+++ magento-plugin/app/code/community/Brera/MagentoConnector/Model/Observer.php
@@ -5,42 +5,37 @@ class Brera_MagentoConnector_Model_Observer
     public function catalogProductSaveAfter(Varien_Event_Observer $observer)
     {
         $productId = $observer->getProduct()->getId();
-        $productQueue = Mage::getModel('brera_magentoconnector/product_queue_item');
-        $productQueue->setAction(Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_CREATE_AND_UPDATE)
-            ->setProductId($productId)
-            ->save();
+        $this->logProductAction(
+            array($productId),
+            Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_CREATE_AND_UPDATE
+        );
     }
 
     public function catalogProductDeleteAfter(Varien_Event_Observer $observer)
     {
-        $this->logDeletedProduct($observer);
+        $productId = $observer->getProduct()->getId();
+        $this->logProductAction(array($productId), Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_DELETE);
     }
 
     public function catalogProductAttributeUpdateAfter(Varien_Event_Observer $observer)
     {
         $productIds = $observer->getProductIds();
-        $productQueue = Mage::getModel('brera_magentoconnector/product_queue_item');
-        $productQueue->saveProductIds(
-            $productIds,
-            Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_CREATE_AND_UPDATE
-        );
+        $this->logProductAction($productIds, Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_CREATE_AND_UPDATE);
     }
 
     public function catalogControllerProductDelete(Varien_Event_Observer $observer)
     {
-        $this->logDeletedProduct($observer);
+        $productId = $observer->getProduct()->getId();
+        $this->logProductAction(array($productId), Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_DELETE);
     }
 
     /**
-     * @param Varien_Event_Observer $observer
-     * @throws Exception
+     * @param int[] $productIds
+     * @param string $action
      */
-    private function logDeletedProduct(Varien_Event_Observer $observer)
+    public function logProductAction($productIds, $action)
     {
-        $productId = $observer->getProduct()->getId();
         $productQueue = Mage::getModel('brera_magentoconnector/product_queue_item');
-        $productQueue->setAction(Brera_MagentoConnector_Model_Product_Queue_Item::ACTION_DELETE)
-            ->setProductId($productId)
-            ->save();
+        $productQueue->saveProductIds($productIds, $action);
     }
 }
