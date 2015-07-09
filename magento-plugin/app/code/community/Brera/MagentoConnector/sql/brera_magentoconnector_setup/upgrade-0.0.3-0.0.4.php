35a524e3c6cd3e836d8121c7ec32d9826b335e3e
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-07-09 17:47:32 +0200

    Issue #16: Make sure sku is linked to product sku too

---
+++ magento-plugin/app/code/community/Brera/MagentoConnector/sql/brera_magentoconnector_setup/upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+$tableName = $this->getTable('brera_magentoconnector/product_queue');
+
+$this->getConnection()->changeColumn($tableName, 'product_id', 'product_id',
+    [
+        'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
+        'unsigned' => true,
+        'nullable' => true,
+        'comment' => 'Product ID',
+    ]
+);
+
+$this->getConnection()->addForeignKey(
+    $this->getIdxName(
+        $tableName,
+        [
+            'product_sku'
+        ]
+    ),
+    $tableName,
+    'product_sku',
+    'catalog/product',
+    'entity_id'
+);
+
+$this->endSetup();
