2f803a710fb77355a1ae618c3bfa6352d186d27b
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-07-19 17:23:28 +0200

    Issue #2: Refactor test

--- lib/Brera/tests/XmlBuilder/ProductTest.php
+++ lib/Brera/tests/XmlBuilder/ProductTest.php
@@ -6,20 +6,15 @@ class XmlBuilderTest extends \PHPUnit_Framework_TestCase
 {
     const XML_START = '<?xml version="1.0" encoding="utf-8"?>';
 
-    /**
-     * @var XmlBuilder
-     */
-    private $xmlBuilder;
-
-    protected function setUp()
-    {
-        $this->xmlBuilder = new XmlBuilder([], []);
-    }
-
-
     public function testProductBuildsEmptyXml()
     {
-        $xml = $this->xmlBuilder->getXmlString();
+        $xmlBuilder = new XmlBuilder([], []);
+        $xml = $xmlBuilder->getXmlString();
         $this->assertEquals(self::XML_START, $xml);
+    }
+
+    public function testXmlWithProductNode()
+    {
+        $productDate = array();
     }
 }
