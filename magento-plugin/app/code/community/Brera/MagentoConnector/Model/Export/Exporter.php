339146ba9ba992d91cc4e54276ec30da83073fe0
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-07-30 21:46:29 +0200

    Issue #27: add error reporting

--- magento-plugin/app/code/community/Brera/MagentoConnector/Model/Export/Exporter.php
+++ magento-plugin/app/code/community/Brera/MagentoConnector/Model/Export/Exporter.php
@@ -21,5 +21,18 @@ class Brera_MagentoConnector_Model_Export_Exporter
         $target = Mage::getStoreConfig('brera/magentoconnector/product_xml_target');
         $uploader = new Brera_MagentoConnector_Model_XmlUploader($xmlString, $target);
         $uploader->upload();
+
+        try {
+            $apiUrl = Mage::getStoreConfig('brera/magentoconnector/api_url');
+            $remoteFileLocation = Mage::getStoreConfig('brera/magentoconnector/remote_catalog_xml_location');
+            $api = new \Brera\MagentoConnector\Api\Api($apiUrl);
+            $api->triggerProductImport($remoteFileLocation);
+        } catch (Exception $e) {
+            Mage::getSingleton('core/session')->addError('Export failed: ' . $e->getMessage());
+
+            return;
+        }
+
+        Mage::getSingleton('core/session')->addSuccess('Export was successfull.');
     }
 }
