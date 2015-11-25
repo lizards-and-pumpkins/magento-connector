28e6ddd710a108dac3b2a7d7dc780aaf6e7a3190
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-11-25 15:19:57 +0100

    Issue #68: Remove install scripts which try to create a product_queue

---
+++ magento-plugin/app/code/community/Brera/MagentoConnector/sql/brera_magentoconnector_setup/install-0.0.5.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+$this->getConnection()->dropTable('brera_product_queue');
+
+$zendQueueInstallQuery = file_get_contents(Mage::getBaseDir('base') . '/lib/Zend/Queue/Adapter/Db/mysql.sql');
+
+$this->getConnection()->multiQuery($zendQueueInstallQuery);
+
+$this->endSetup();
