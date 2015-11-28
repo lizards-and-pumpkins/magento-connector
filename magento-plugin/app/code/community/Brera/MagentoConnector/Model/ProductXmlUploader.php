5f2a4439bba2e8c256a5fdb22e921597068502fa
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-11-28 23:28:03 +0100

    Issue #81: Fixes #81 make it possible to use strftime variables in export filename

--- magento-plugin/app/code/community/Brera/MagentoConnector/Model/ProductXmlUploader.php
+++ magento-plugin/app/code/community/Brera/MagentoConnector/Model/ProductXmlUploader.php
@@ -4,7 +4,7 @@ class Brera_MagentoConnector_Model_ProductXmlUploader extends Brera_MagentoConne
 {
     public function __construct()
     {
-        parent::__construct(Mage::getStoreConfig('brera/magentoconnector/product_xml_target'));
+        parent::__construct(strftime(Mage::getStoreConfig('brera/magentoconnector/product_xml_target')));
     }
 
 }
