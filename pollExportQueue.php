5a7cdbb5731a6e043f7f9e566483f687f4018ec2
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-11-29 18:57:20 +0100

    Issue #85: Fixes #85

---
+++ pollExportQueue.php
@@ -0,0 +1,9 @@
+<?php
+require 'lib/autoload_brera.php';
+require 'app/Mage.php';
+Mage::app();
+
+while (true) {
+    $exporter = Mage::getModel('brera_magentoconnector/export_exporter');
+    $exporter->exportProductsInQueue();
+}
