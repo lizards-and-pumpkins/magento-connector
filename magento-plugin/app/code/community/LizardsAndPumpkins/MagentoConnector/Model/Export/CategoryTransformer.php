79561ab5211f154ca2302a877ee53e44a9a54ccf
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2015-12-10 13:09:17 +0100

    Issue #122: Revert category transformer

--- magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/Model/Export/CategoryTransformer.php
+++ magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/Model/Export/CategoryTransformer.php
@@ -1,131 +1,77 @@
 <?php
 
-use LizardsAndPumpkins\MagentoConnector\Api\Api;
-use LizardsAndPumpkins\MagentoConnector\XmlBuilder\CatalogMerge;
+use LizardsAndPumpkins\MagentoConnector\XmlBuilder\ListingBuilder;
 
-class LizardsAndPumpkins_MagentoConnector_Model_Export_CatalogExporter
+class LizardsAndPumpkins_MagentoConnector_Model_Export_CategoryTransformer
 {
-    private $numberOfProductsExported = 0;
-    private $numberOfCategoriesExported = 0;
+    /**
+     * @var Mage_Catalog_Model_Category
+     */
+    private $category;
 
     /**
-     * @var Mage_Core_Model_Session
+     * @var LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig
      */
-    private $coreSession;
+    private $config;
 
     /**
-     * @var LizardsAndPumpkins_MagentoConnector_Model_Export_ProductCollector
+     * @param Mage_Catalog_Model_Category                                    $category
+     * @param LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig $config
      */
-    private $productCollector;
-
-    public function __construct()
-    {
-        $this->productCollector = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_productCollector');
-        $this->coreSession = Mage::getSingleton('core/session');
+    private function __construct(
+        Mage_Catalog_Model_Category $category,
+        LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig $config = null
+    ) {
+        $this->category = $category;
+        $this->config = $config;
     }
 
-    public function exportAllProducts()
+    /**
+     * @param Mage_Catalog_Model_Category                                         $category
+     * @param LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig|null $config
+     * @return LizardsAndPumpkins_MagentoConnector_Model_Export_CategoryTransformer
+     */
+    public static function createForTesting(
+        Mage_Catalog_Model_Category $category,
+        LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig $config = null
+    ) {
+        return new self($category, $config);
+    }
+
+    /**
+     * @param Mage_Catalog_Model_Category $category
+     * @return LizardsAndPumpkins_MagentoConnector_Model_Export_CategoryTransformer
+     */
+    public static function createFrom(Mage_Catalog_Model_Category $category)
     {
-        $helper = Mage::helper('lizardsAndPumpkins_magentoconnector/export');
-        $helper->addAllProductIdsToProductUpdateQueue();
-        $helper->addAllCategoryIdsToCategoryQueue();
-        $this->exportProductsInQueue();
+        $config = new LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig();
+        return new self($category, $config);
+    }
+
+    /**
+     * @return \LizardsAndPumpkins\MagentoConnector\XmlBuilder\XmlString
+     */
+    public function getCategoryXml()
+    {
+        if (!($this->category->getStore() instanceof Mage_Core_Model_Store)) {
+            throw new RuntimeException('Store must be set on category.');
+        }
+
+        $urlPath = $this->category->getUrlPath();
+        $listingBuilder = ListingBuilder::create($urlPath, 'and');
+        $listingBuilder->addFilterCriterion('category', 'Equal', $urlPath);
+        $listingBuilder->setLocale($this->getLocale($this->category->getStore()));
+        $listingBuilder->setWebsite($this->category->getStore()->getWebsite()->getCode());
+
+        return $listingBuilder->buildXml();
     }
 
     /**
      * @param Mage_Core_Model_Store $store
-     * @return int
+     * @return string
      */
-    public function exportOneStore(Mage_Core_Model_Store $store)
+    private function getLocale($store)
     {
-        Mage::helper('lizardsAndPumpkins_magentoconnector/export')
-            ->addAllProductIdsFromWebsiteToProductUpdateQueue($store->getWebsite());
-        $collector = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_productCollector');
-        $collector->setStoresToExport([$store]);
-        $this->export($collector);
-    }
-
-    /**
-     * @param Mage_Core_Model_Website $website
-     * @return int
-     */
-    public function exportOneWebsite(Mage_Core_Model_Website $website)
-    {
-        Mage::helper('lizardsAndPumpkins_magentoconnector/export')
-            ->addAllProductIdsFromWebsiteToProductUpdateQueue($website);
-        $collector = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_productCollector');
-        $collector->setStoresToExport($website->getStores());
-        $this->export($collector);
-    }
-
-    public function exportProductsInQueue()
-    {
-        $collector = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_productCollector');
-        return $this->export($collector);
-    }
-
-    /**
-     * @param string $filename
-     */
-    private function triggerCatalogUpdateApi($filename)
-    {
-        $apiUrl = Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url');
-        $api = new Api($apiUrl);
-        $api->triggerProductImport($filename);
-    }
-
-    /**
-     * @param LizardsAndPumpkins_MagentoConnector_Model_Export_ProductCollector $collector
-     * @return int
-     */
-    private function export(LizardsAndPumpkins_MagentoConnector_Model_Export_ProductCollector $collector)
-    {
-        $xmlMerge = new CatalogMerge();
-        /** @var LizardsAndPumpkins_MagentoConnector_Model_ProductXmlUploader $uploader */
-        $uploader = Mage::getModel('lizardsAndPumpkins_magentoconnector/productXmlUploader');
-
-        while ($product = $collector->getProduct()) {
-            $xmlBuilderAndUploader = new LizardsAndPumpkins_MagentoConnector_Model_Export_ProductXmlBuilderAndUploader(
-                $product,
-                $xmlMerge,
-                $uploader
-            );
-
-            $xmlBuilderAndUploader->process();
-            $this->numberOfProductsExported++;
-        }
-
-        $categoryCollector = new LizardsAndPumpkins_MagentoConnector_Model_Export_CategoryCollector();
-
-        while ($category = $categoryCollector->getCategory()) {
-            $transformer = LizardsAndPumpkins_MagentoConnector_Model_Export_CategoryTransformer::createFrom($category);
-            $categoryXml = $transformer->getCategoryXml();
-            $xmlMerge->addCategory($categoryXml);
-            $this->numberOfCategoriesExported++;
-        }
-        
-        if (0 === ($this->numberOfProductsExported + $this->numberOfCategoriesExported)) {
-            return;
-        }
-
-        $uploader->writePartialXmlString($xmlMerge->finish());
-        $filename = $uploader->getFilename();
-        $this->triggerCatalogUpdateApi($filename);
-    }
-
-    /**
-     * @return int
-     */
-    public function getNumberOfCategoriesExported()
-    {
-        return $this->numberOfCategoriesExported;
-    }
-
-    /**
-     * @return int
-     */
-    public function getNumberOfProductsExported()
-    {
-        return $this->numberOfProductsExported;
+        return $this->config->getLocaleFrom($store);
     }
 }
