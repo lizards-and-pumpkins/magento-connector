301a269dbc111c1245bceb39dcaf7dc8c155b48f
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-15 23:32:39 +0100

    Issue #153: Separate api call from export

--- magento-plugin/shell/lizards_and_pumpkins_export.php
+++ magento-plugin/shell/lizards_and_pumpkins_export.php
@@ -19,18 +19,28 @@ class LizardsAndPumpkins_Export extends Mage_Shell_Abstract
         /** @var LizardsAndPumpkins_MagentoConnector_Model_Export_CatalogExporter $exporter */
         $exporter = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_catalogExporter');
         if ($this->getArg('all-products')) {
-            $exporter->exportAllProducts();
+            $filename = $exporter->exportAllProducts();
+            $this->triggerCatalogUpdateApi($filename);
         } elseif ($this->getArg('queued-products')) {
-            $exporter->exportProductsInQueue();
+            $filename = $exporter->exportProductsInQueue();
+            $this->triggerCatalogUpdateApi($filename);
         } elseif ($this->getArg('queued-categories')) {
-            $exporter->exportCategoriesInQueue();
+            $filename = $exporter->exportCategoriesInQueue();
+            $this->triggerCatalogUpdateApi($filename);
         } elseif ($this->getArg('all-categories')) {
-            $exporter->exportAllCategories();
+            $filename = $exporter->exportAllCategories();
+            $this->triggerCatalogUpdateApi($filename);
         } elseif ($this->getArg('stats')) {
             $this->outputStatistics();
         } else {
             echo $this->usageHelp();
         }
+    }
+
+    private function triggerCatalogUpdateApi($filename)
+    {
+        $apiUrl = Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url');
+        (new Api($apiUrl))->triggerProductImport($filename);
     }
 
     /**
--- pollExportQueue.php
+++ pollExportQueue.php
@@ -1,10 +1,17 @@
 <?php
+
+use LizardsAndPumpkins\MagentoConnector\Api\Api;
+
 require 'lib/autoload_lizards_and_pumpkins.php';
 require 'app/Mage.php';
 Mage::app();
 
 while (true) {
     $exporter = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_catalogExporter');
-    $exporter->exportProductsInQueue();
+    $filename = $exporter->exportProductsInQueue();
+    
+    $apiUrl = Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url');
+    (new Api($apiUrl))->triggerProductImport($filename);
+    
     usleep(500000);
 }
