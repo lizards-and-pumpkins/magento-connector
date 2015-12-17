11bd78f84ab8ae782a63a41458714b117c40dace
Vinai Kopp <vinai@netzarbeiter.com>
2015-12-17 18:25:22 +0100

    Issue #156: Add switch to add all products to queue

--- tests/Integration/util/addProductIdsToQueue.php
+++ tests/Integration/util/addProductIdsToQueue.php
@@ -13,29 +13,38 @@ EOM;
     exit(2);
 }
 
-$numberOfProducts = (int) $argv[1];
-if (! ($numberOfProducts > 0)) {
-    echo <<< EOM
+if ('all' === $argv[1]) {
+    
+    $helper = Mage::helper('lizardsAndPumpkins_magentoconnector/export');
+    $helper->addAllProductIdsToProductUpdateQueue();
+    echo "Added all product ids to the export queue\n";
+    
+} else {
+
+    $numberOfProducts = (int) $argv[1];
+    if ($numberOfProducts <= 0) {
+        echo <<< EOM
 Specify a number larger then zero to be added to the queue.
 
 EOM;
-    exit(2);
+        exit(2);
+    }
+    
+    $collection = Mage::getResourceModel('catalog/product_collection');
+    $collection->addAttributeToFilter(
+        'visibility',
+        ['neq' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE]
+    );
+    $collection->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
+
+    $select = $collection->getSelect();
+    $select->reset(Zend_Db_Select::COLUMNS);
+    $select->columns('entity_id');
+    $select->limit($numberOfProducts);
+
+    Mage::helper('lizardsAndPumpkins_magentoconnector/export')->addProductUpdatesToQueue(
+        $collection->getConnection()->fetchCol($select)
+    );
+
+    echo "Added {$numberOfProducts} product id(s) to the export queue\n";
 }
-
-$collection = Mage::getResourceModel('catalog/product_collection');
-$collection->addAttributeToFilter(
-    'visibility',
-    ['neq' => Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE]
-);
-$collection->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
-
-$select = $collection->getSelect();
-$select->reset(Zend_Db_Select::COLUMNS);
-$select->columns('entity_id');
-$select->limit($numberOfProducts);
-
-Mage::helper('lizardsAndPumpkins_magentoconnector/export')->addProductUpdatesToQueue(
-    $collection->getConnection()->fetchCol($select)
-);
-
-echo "Added {$numberOfProducts} product id(s) to the export queue\n";
