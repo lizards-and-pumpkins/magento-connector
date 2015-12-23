8f479507da17bfcd821e91d5e0298a396ce0f5a2
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-12-23 22:19:00 +0100

    Issue #173: Add trait to remove directory with files and subdirs

---
+++ tests/Unit/Suites/RemoveDirectory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LizardsAndPumpkins\MagentoConnector;
+
+trait RemoveDirectory
+{
+    /**
+     * @param string $dir
+     */
+    private function removeDirectoryRecursivly($dir)
+    {
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                rmdir((string) $file);
+            } else {
+                unlink((string) $file);
+            }
+        }
+        rmdir($dir);
+    }
+}
