4801f5393d5836468540f6dcb848602583eeeeda
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-12-29 19:37:12 +0100

    Issue #200: Add fixed category helper

---
+++ magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/Helper/Catalog/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+class LizardsAndPumpkins_MagentoConnector_Helper_Catalog_Category extends Mage_Catalog_Helper_Category
+{
+
+    /**
+     * @param bool $sorted
+     * @param bool $asCollection
+     * @param bool $toLoad
+     * @return array|Mage_Catalog_Model_Resource_Eav_Mysql4_Category_Collection|Varien_Data_Tree_Node_Collection
+     */
+    public function getStoreCategories($sorted = false, $asCollection = false, $toLoad = true)
+    {
+        $categories = parent::getStoreCategories(
+            $sorted, $asCollection, $toLoad
+        );
+
+        $this->deleteCacheBecauseEntriesMightBeInWrongLanguage();
+
+        return $categories;
+    }
+
+    private function deleteCacheBecauseEntriesMightBeInWrongLanguage()
+    {
+        $this->_storeCategories = [];
+    }
+
+}
