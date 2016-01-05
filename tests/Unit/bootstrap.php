ee7aae9b8df36df8af347dcc9a011107f250fa06
Vinai Kopp <vinai@netzarbeiter.com>
2016-01-05 11:27:13 +0100

    Issue #215: Expand autoloader to also use namespace based file location (psr-4)

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
+
 error_reporting(E_ALL);
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 spl_autoload_register(function ($classname) {
-    $classnameReplaced = str_replace('_', '/', $classname);
-    if (is_file('magento-plugin/app/code/community/' . $classnameReplaced . '.php')) {
-        require 'magento-plugin/app/code/community/' . $classnameReplaced . '.php';
+    $classnameReplaced = str_replace(['_', '\\'], '/', $classname);
+    $filename = __DIR__ . '/../../magento-plugin/app/code/community/' . $classnameReplaced . '.php';
+    if (is_file($filename)) {
+        require $filename;
     }
 });
