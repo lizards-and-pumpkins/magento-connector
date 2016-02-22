c229c20a50bacd89c24ba0debbbdd946a1b3bb02
Vinai Kopp <vinai@netzarbeiter.com>
2016-02-22 13:47:17 +0100

    Issue #263: Expire pollExportQueue process after N iterations

--- pollExportQueue.php
+++ pollExportQueue.php
@@ -7,14 +7,26 @@ require __DIR__ . '/vendor/autoload.php';
 require 'app/Mage.php';
 Mage::app();
 
-do {
-    /** @var LizardsAndPumpkins_MagentoConnector_Model_Export_CatalogExporter $exporter */
-    $exporter = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_catalogExporter');
-    $filename = $exporter->exportProductsInQueue();
-    if ($exporter->wasSomethingExported()) {
-        $apiUrl = Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url');
-        (new Api($apiUrl))->triggerProductImport($filename);
-    }
+class PollsExportQueue
+{
+    private static $sleepMicroSeconds =  500000;
+    private static $iterationsUntilExit = 200;
+    
+    public static function run()
+    {
+        $iteration = 0;
+        do {
+            /** @var LizardsAndPumpkins_MagentoConnector_Model_Export_CatalogExporter $exporter */
+            $exporter = Mage::getModel('lizardsAndPumpkins_magentoconnector/export_catalogExporter');
+            $filename = $exporter->exportProductsInQueue();
+            if ($exporter->wasSomethingExported()) {
+                $apiUrl = Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url');
+                (new Api($apiUrl))->triggerProductImport($filename);
+            }
 
-    usleep(500000);
-} while (true);
+            usleep(self::$sleepMicroSeconds);
+        } while ($iteration++ < self::$iterationsUntilExit);
+    }
+}
+
+PollsExportQueue::run();
