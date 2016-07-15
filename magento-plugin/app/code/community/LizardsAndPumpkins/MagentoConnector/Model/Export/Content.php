b5601291fda1304cfb3823076a309778fce189e1
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-07-15 13:15:04 +0200

    Issue #309: Fix exporting CMS blocks assigned to all store views

--- magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/Model/Export/Content.php
+++ magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/Model/Export/Content.php
@@ -104,25 +104,41 @@ class LizardsAndPumpkins_MagentoConnector_Model_Export_Content
 
     private function exportCmsBlock(Mage_Cms_Model_Block $block)
     {
-        $blockId = $this->normalizeIdentifier($block->getIdentifier());
-        $context = $this->getBlockContext($block);
-        $keyGeneratorParameters = [];
+        if ($block->getData('store_id') !== '0') {
+            $blockId = $this->normalizeIdentifier($block->getIdentifier());
+            $context = $this->getBlockContext($block);
+            $keyGeneratorParameters = [];
 
-        $this->getApi()->triggerCmsBlockUpdate($blockId, $block->getContent(), $context, $keyGeneratorParameters);
+            $this->getApi()->triggerCmsBlockUpdate($blockId, $block->getContent(), $context, $keyGeneratorParameters);
+            return;
+        }
+
+        array_map(function(Mage_Core_Model_Store $store) use ($block) {
+            $block->setData('store_id', $store->getId());
+            $this->exportCmsBlock($block);
+        }, Mage::app()->getStores());
     }
 
     private function exportProductListingCmsBlock(Mage_Cms_Model_Block $block)
     {
-        $blockIdStringWithoutLastVariableToken = preg_replace('/_[^_]+$/', '', $block->getIdentifier());
+        if ($block->getData('store_id') !== '0') {
+            $blockIdStringWithoutLastVariableToken = preg_replace('/_[^_]+$/', '', $block->getIdentifier());
 
-        $categoryUrlSuffix = Mage::getStoreConfig(Mage_Catalog_Helper_Category::XML_PATH_CATEGORY_URL_SUFFIX);
-        $categorySlug = preg_replace('/.*_/', '', $block->getIdentifier()) . '.' . $categoryUrlSuffix;
-        $keyGeneratorParameters = ['url_key' => $categorySlug];
+            $categoryUrlSuffix = Mage::getStoreConfig(Mage_Catalog_Helper_Category::XML_PATH_CATEGORY_URL_SUFFIX);
+            $categorySlug = preg_replace('/.*_/', '', $block->getIdentifier()) . '.' . $categoryUrlSuffix;
+            $keyGeneratorParameters = ['url_key' => $categorySlug];
 
-        $blockId = $this->normalizeIdentifier($blockIdStringWithoutLastVariableToken);
-        $context = $this->getBlockContext($block);
+            $blockId = $this->normalizeIdentifier($blockIdStringWithoutLastVariableToken);
+            $context = $this->getBlockContext($block);
 
-        $this->getApi()->triggerCmsBlockUpdate($blockId, $block->getContent(), $context, $keyGeneratorParameters);
+            $this->getApi()->triggerCmsBlockUpdate($blockId, $block->getContent(), $context, $keyGeneratorParameters);
+            return;
+        }
+
+        array_map(function(Mage_Core_Model_Store $store) use ($block) {
+            $block->setData('store_id', $store->getId());
+            $this->exportProductListingCmsBlock($block);
+        }, Mage::app()->getStores());
     }
 
     /**
