526f6f367ad0e82240ecf8616bdb3d54daef4a1d
Tim Bezhashvyly <tim.bezhashvyly@gmail.com>
2016-09-12 13:49:37 +0200

    Issue #320: Remove dependency on PrototypeJS in info.js

--- magento-plugin/skin/frontend/base/default/js/lizardsAndPumpkins_magentoconnector/session/info.js
+++ magento-plugin/skin/frontend/base/default/js/lizardsAndPumpkins_magentoconnector/session/info.js
@@ -1,5 +1,19 @@
-Event.observe(document, 'dom:loaded', function () {
+function setLizardsAndPumpkinsCookie() {
     if (typeof lizardsAndPumpkinsTransport != "undefined") {
         Mage.Cookies.set('lizardsAndPumpkinsTransport', JSON.stringify(lizardsAndPumpkinsTransport), null, '/');
     }
-});
+}
+
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function () {
+        //document.removeEventListener( "DOMContentLoaded", arguments.callee, false);
+        setLizardsAndPumpkinsCookie();
+    }, false);
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function () {
+        if ("complete" === document.readyState) {
+            //document.detachEvent( "onreadystatechange", arguments.callee );
+            setLizardsAndPumpkinsCookie();
+        }
+    });
+}
