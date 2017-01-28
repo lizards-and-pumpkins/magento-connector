97bf8b18ef533e2bb523d33a4823f5ed98b62d7f
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-01-28 17:52:15 +0100

    Issue #328: Add validation of status code

--- lib/LizardsAndPumpkins/src/Api/GuzzleAdapter.php
+++ lib/LizardsAndPumpkins/src/Api/GuzzleAdapter.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace LizardsAndPumpkins\MagentoConnector\Api;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7\Request;
 
 class GuzzleAdapter implements HttpApiClient
@@ -22,13 +24,29 @@ class GuzzleAdapter implements HttpApiClient
     public function putRequest(string $url, string $body, array $headers): string
     {
         $this->validateUrl($url);
-        return (string)$this->client->send(new Request('PUT', $url, $headers, $body))->getBody();
+        $method = 'PUT';
+        $response = $this->sendRequest($url, $method, $body, $headers);
+
+        if ($response->getStatusCode() !== 202) {
+            throw new RequestFailedException(
+                sprintf('Status code %s does not match expected 202.', $response->getStatusCode())
+            );
+        }
+        return (string)$response->getBody();
     }
 
     public function getRequest(string $url, string $body, array $headers): string
     {
         $this->validateUrl($url);
-        return (string)$this->client->send(new Request('GET', $url, $headers, $body))->getBody();
+        $method = 'GET';
+        $response = $this->sendRequest($url, $method, $body, $headers);
+
+        if ($response->getStatusCode() !== 200) {
+            throw new RequestFailedException(
+                sprintf('Status code %s does not match expected 200.', $response->getStatusCode())
+            );
+        }
+        return (string)$response->getBody();
     }
 
     private function validateUrl(string $url)
@@ -49,5 +67,24 @@ class GuzzleAdapter implements HttpApiClient
         if ($parts['scheme'] !== 'http' && $parts['scheme'] !== 'https') {
             throw new InvalidHostException('Url must either be http or https.');
         }
+    }
+
+    /**
+     * @param string $url
+     * @param        $method
+     * @param string $body
+     * @param array  $headers
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     */
+    private function sendRequest(string $url, $method, string $body, array $headers)
+    {
+        try {
+            $response = $this->client->send(new Request($method, $url, $headers, $body));
+        } catch (ClientException $e) {
+            throw new RequestFailedException($e->getMessage(), $e->getCode(), $e);
+        } catch (ServerException $e) {
+            throw new RequestFailedException($e->getMessage(), $e->getCode(), $e);
+        }
+        return $response;
     }
 }
--- tests/Unit/Suites/Api/GuzzleAdapterTest.php
+++ tests/Unit/Suites/Api/GuzzleAdapterTest.php
@@ -85,6 +85,123 @@ class GuzzleAdapterTest extends \PHPUnit_Framework_TestCase
         }
     }
 
+    /**
+     * @dataProvider provideNon200StatusCodes
+     * @param int $statusCode
+     */
+    public function testThrowsExceptionOnNon200GetResponse(int $statusCode)
+    {
+        $this->expectException(RequestFailedException::class);
+        $url = 'http://example.com/123';
+        $body = '';
+        $headers = [];
+        $responseBody = '';
+
+        $this->mockHandler->append(new Response($statusCode, [], $responseBody));
+
+        $this->client->getRequest($url, $body, $headers);
+    }
+
+    public function provideNon200StatusCodes(): array
+    {
+        return $this->provideStatusCodes([200]);
+    }
+
+    /**
+     * @dataProvider provideNon202StatusCodes
+     * @param int $statusCode
+     */
+    public function testThrowsExceptionOnNon202PutResponse(int $statusCode)
+    {
+        $this->expectException(RequestFailedException::class);
+        $url = 'http://example.com/123';
+        $body = '';
+        $headers = [];
+        $responseBody = '';
+
+        $this->mockHandler->append(new Response($statusCode, [], $responseBody));
+
+        $this->client->putRequest($url, $body, $headers);
+    }
+
+    public function provideNon202StatusCodes(): array
+    {
+        return $this->provideStatusCodes([202]);
+    }
+
+    private function provideStatusCodes(array $except = []): array
+    {
+        $statusCodes = [
+            'Continue'                           => [100],
+            'Switching Protocols'                => [101],
+            'Processing'                         => [102],
+            'OK'                                 => [200],
+            'Created'                            => [201],
+            'Accepted'                           => [202],
+            'Non-authoritative Information'      => [203],
+            'No Content'                         => [204],
+            'Reset Content'                      => [205],
+            'Partial Content'                    => [206],
+            'Multi-Status'                       => [207],
+            'Already Reported'                   => [208],
+            'IM Used'                            => [226],
+            'Multiple Choices'                   => [300],
+            'Moved Permanently'                  => [301],
+            'Found'                              => [302],
+            'See Other'                          => [303],
+            'Not Modified'                       => [304],
+            'Use Proxy'                          => [305],
+            'Temporary Redirect'                 => [307],
+            'Permanent Redirect'                 => [308],
+            'Bad Request'                        => [400],
+            'Unauthorized'                       => [401],
+            'Payment Required'                   => [402],
+            'Forbidden'                          => [403],
+            'Not Found'                          => [404],
+            'Method Not Allowed'                 => [405],
+            'Not Acceptable'                     => [406],
+            'Proxy Authentication Required'      => [407],
+            'Request Timeout'                    => [408],
+            'Conflict'                           => [409],
+            'Gone'                               => [410],
+            'Length Required'                    => [411],
+            'Precondition Failed'                => [412],
+            'Payload Too Large'                  => [413],
+            'Request-URI Too Long'               => [414],
+            'Unsupported Media Type'             => [415],
+            'Requested Range Not Satisfiable'    => [416],
+            'Expectation Failed'                 => [417],
+            'I\'m a teapot'                      => [418],
+            'Misdirected Request'                => [421],
+            'Unprocessable Entity'               => [422],
+            'Locked'                             => [423],
+            'Failed Dependency'                  => [424],
+            'Upgrade Required'                   => [426],
+            'Precondition Required'              => [428],
+            'Too Many Requests'                  => [429],
+            'Request Header Fields Too Large'    => [431],
+            'Connection Closed Without Response' => [444],
+            'Unavailable For Legal Reasons'      => [451],
+            'Client Closed Request'              => [499],
+            'Internal Server Error'              => [500],
+            'Not Implemented'                    => [501],
+            'Bad Gateway'                        => [502],
+            'Service Unavailable'                => [503],
+            'Gateway Timeout'                    => [504],
+            'HTTP Version Not Supported'         => [505],
+            'Variant Also Negotiates'            => [506],
+            'Insufficient Storage'               => [507],
+            'Loop Detected'                      => [508],
+            'Not Extended'                       => [510],
+            'Network Authentication Required'    => [511],
+            'Network Connect Timeout Error'      => [599],
+        ];
+
+        return array_filter($statusCodes, function ($arrayCode) use ($except) {
+            return !in_array($arrayCode[0], $except, true);
+        });
+    }
+
     public function testPutRequest()
     {
         $url = 'http://example.com/123';
@@ -97,7 +214,7 @@ class GuzzleAdapterTest extends \PHPUnit_Framework_TestCase
         ];
         $responseBody = 'RESPONSE-BODY';
 
-        $this->mockHandler->append(new Response(200, [], $responseBody));
+        $this->mockHandler->append(new Response(202, [], $responseBody));
 
         $response = $this->client->putRequest($url, $body, $headers);
 
