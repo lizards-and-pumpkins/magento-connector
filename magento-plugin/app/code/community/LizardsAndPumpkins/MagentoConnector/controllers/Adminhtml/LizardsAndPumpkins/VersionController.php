0f27c66029c499373f5ef5108a3b65b33895af50
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-01-28 23:08:14 +0100

    Issue #328: Add API to Form controller

--- magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/controllers/Adminhtml/LizardsAndPumpkins/VersionController.php
+++ magento-plugin/app/code/community/LizardsAndPumpkins/MagentoConnector/controllers/Adminhtml/LizardsAndPumpkins/VersionController.php
@@ -2,12 +2,35 @@
 
 declare(strict_types=1);
 
+use LizardsAndPumpkins\MagentoConnector\Api\Api;
+use LizardsAndPumpkins\MagentoConnector\Api\GuzzleAdapter;
+
 class LizardsAndPumpkins_MagentoConnector_Adminhtml_LizardsAndPumpkins_VersionController
     extends Mage_Adminhtml_Controller_Action
 {
+    /**
+     * @var Api
+     */
+    private $api;
+
+    public function __construct(
+        Zend_Controller_Request_Abstract $request,
+        Zend_Controller_Response_Abstract $response,
+        array $invokeArgs = [],
+        Api $api = null
+    ) {
+        parent::__construct($request, $response, $invokeArgs);
+        $this->api = $api ?? new Api(
+                Mage::getStoreConfig('lizardsAndPumpkins/magentoconnector/api_url'),
+                new GuzzleAdapter()
+            );
+    }
+
     public function indexAction()
     {
+        $version = $this->api->getCurrentVersion();
         $this->loadLayout();
+        $this->getLayout()->getBlock('version.container')->getChild('form')->setVersion($version);
         $this->renderLayout();
     }
 }
--- tests/Integration/Suites/Controller/LizardsAndPumpkins/VersionControllerTest.php
+++ tests/Integration/Suites/Controller/LizardsAndPumpkins/VersionControllerTest.php
@@ -2,7 +2,79 @@
 
 declare(strict_types=1);
 
+use LizardsAndPumpkins\MagentoConnector\Api\Api;
+
+require Mage::getBaseDir('app')
+    . '/code/community/LizardsAndPumpkins/MagentoConnector/controllers/Adminhtml/LizardsAndPumpkins/VersionController.php';
+
 class VersionControllerTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Api|PHPUnit_Framework_MockObject_MockObject
+     */
+    private $api;
 
+    /**
+     * @var LizardsAndPumpkins_MagentoConnector_Adminhtml_LizardsAndPumpkins_VersionController
+     */
+    private $controller;
+
+    protected function setUp()
+    {
+        $sessionMock = $this->getMockBuilder(Mage_Admin_Model_Session::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['isAllowed', 'getUser'])
+            ->getMock();
+        $sessionMock->method('isAllowed')->willReturn(true);
+        $sessionMock->method('getUser')->willReturn($this->createMock(Mage_Admin_Model_User::class));
+
+        Mage::register('_singleton/admin/session', $sessionMock);
+
+        $this->api = $this->createMock(Api::class);
+        $request = $this->getMockBuilder(Zend_Controller_Request_Http::class)
+            ->disableOriginalConstructor()
+            ->setMethods(
+                [
+                    'getRequestedRouteName',
+                    'getRequestedControllerName',
+                    'getRequestedActionName',
+                    'getRouteName',
+                    'getControllerName',
+                    'getActionName',
+                    'isDispatched',
+                ]
+            )
+            ->getMock();
+
+        $request->method('getRouteName')->willReturn('adminhtml');
+        $request->method('getControllerName')->willReturn('lizardsandpumpkins_version');
+        $request->method('getActionName')->willReturn('index');
+        $request->method('getRequestedRouteName')->willReturn('adminhtml');
+        $request->method('getRequestedControllerName')->willReturn('lizardsandpumpkins_version');
+        $request->method('getRequestedActionName')->willReturn('index');
+        $request->method('isDispatched')->willReturn(true);
+
+        $response = $this->createMock(Zend_Controller_Response_Http::class);
+        $this->controller = new LizardsAndPumpkins_MagentoConnector_Adminhtml_LizardsAndPumpkins_VersionController(
+            $request, $response, [], $this->api
+        );
+    }
+
+    public function testGetCurrentVersionAndSetOnBlock()
+    {
+        $version = [
+            'data' => [
+                'current_version'  => '1',
+                'previous_version' => '0',
+            ],
+        ];
+        $this->api->expects($this->once())->method('getCurrentVersion')->willReturn($version);
+
+        $this->controller->dispatch('index');
+
+        $this->assertEquals(
+            $version,
+            $this->controller->getLayout()->getBlock('version.container')->getChild('form')->getVersion()
+        );
+    }
 }
