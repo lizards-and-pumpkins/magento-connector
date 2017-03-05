2be6df42a79dbe1731a433fac1b335f2ac3e1c75
Vinai Kopp <vinai@netzarbeiter.com>
2017-03-05 01:28:38 +0100

    Issue #346: Correctly calculate the expected url key suffix for categories

--- tests/Integration/Suites/Model/Export/SpecificCategoryCollectorTest.php
+++ tests/Integration/Suites/Model/Export/SpecificCategoryCollectorTest.php
@@ -8,11 +8,13 @@ class LizardsAndPumpkins_MagentoConnector_Model_Export_SpecificCategoryCollector
         $config = new LizardsAndPumpkins_MagentoConnector_Model_Export_MagentoConfig();
         $categoryUrlPathSuffix = Mage::getStoreConfig(Mage_Catalog_Helper_Category::XML_PATH_CATEGORY_URL_SUFFIX);
         
+        $expectedSuffix = '.' === $categoryUrlPathSuffix[0] ? $categoryUrlPathSuffix : '.' . $categoryUrlPathSuffix;
+        
         $categoryCollector = new LizardsAndPumpkins_MagentoConnector_Model_Export_SpecificCategoryCollector($config);
         
         $categoryCollector->setCategoryIdsToExport([4]);
         
         $category = $categoryCollector->getCategory();
-        $this->assertStringEndsWith('.' . $categoryUrlPathSuffix, $category->getData('url_path'));
+        $this->assertStringEndsWith($expectedSuffix, $category->getData('url_path'));
     }
 }
